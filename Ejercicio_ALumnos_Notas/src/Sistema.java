17d64f2fed7056be71d2eea277e44ea9bad9980b
KevinDaniel10 <kevin.cola@epn.edu.ec>
2023-11-24 11:50:32 -0500

    Agregandi

---
+++ Ejercicio_ALumnos_Notas/src/Sistema.java
@@ -0,0 +1,70 @@
+public class Sistema {
+    public String CI;
+    public String nombres;
+    public  String apellidos;
+
+    public String materia;
+
+    public Sistema(String CI) {
+        this.CI = CI;
+    }
+
+    public String getCI() {
+        return CI;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public String getMateria() {
+        return materia;
+    }
+
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public void setCI(String CI) {
+        this.CI = CI;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getNota1() {
+        return nota1;
+    }
+
+    public void setNota1(int nota1) {
+        this.nota1 = nota1;
+    }
+
+    public int getNota2() {
+        return nota2;
+    }
+
+    public void setNota2(int nota2) {
+        this.nota2 = nota2;
+    }
+
+    public int nota1;
+    public int nota2;
+    public double calcularPromedio() {
+        return (double) (getNota1() + getNota2() ) / 2;
+    }
+    public boolean quedoEnSupletoria() {
+        return calcularPromedio() < 14;
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # Practica_ALumnos_Materias
+public class Autos2 {
+public String marca;
+public String modelo ;
+public String Agencia ;
+public int costo;
+public int velocidad;
+public int kilometrage;
+
+    public String color;
+
+    public Autos2(String nuevoMarca){
+
+        marca=nuevoMarca;
+    }
+    public String getMarca(){
+        return  marca;
+    }
+    public String getModelo(){
+        return modelo;
+    }
+    public int getVelocidad(){
+        return  velocidad;
+    }
+    public int getCosto(){
+        return costo;
+    }
+    public String getAgencia(){
+
+        return  Agencia;
+    }
+    public String getColor(){
+
+        return color;
+    }
+
+    public void  setModelo(String newmodelo){
+        this.modelo=newmodelo;
+    }
+    public void  setCosto(int newcosto){
+        this.costo=newcosto;
+    }
+    public void  setColor(String newcolor){
+        this.color=newcolor;
+    }
+    public void  setAgencia(String newagencia){
+        this.Agencia=newagencia;
+    }
+    public void  setVelocidad(int newvelocida){
+        this.velocidad=newvelocida;
+    }
+    public void  setKilometrage(int newkilometrage){
+        this.kilometrage=newkilometrage;
+    }
+
+}
