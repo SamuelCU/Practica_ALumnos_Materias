7867bc75572930f984683c05adc85d8e6d3fbe6d
Estefanía Sánchez <estefi2000ms2@gmail.com>
2023-12-01 21:53:58 -0500

    Version 3.0

--- Ejercicio_ALumnos_Notas/src/Main.java
+++ Ejercicio_ALumnos_Notas/src/Main.java
@@ -4,7 +4,6 @@ public class Main {
   public static void main(String[] args) {
     Scanner leer = new Scanner(System.in);
 
-    // Crear un array para almacenar los objetos Sistema
     Sistema[] estudiantes = new Sistema[5];
 
     for (int i = 0; i < estudiantes.length; i++) {
@@ -14,36 +13,44 @@ public class Main {
       int ci = leer.nextInt();
       leer.nextLine(); // Consumir el salto de línea
 
-      // Crear un objeto Sistema para el estudiante actual
+
       Sistema estudiante = new Sistema(ci);
 
-      System.out.print("Ingrese los nombres del estudiante: ");
-      String nombres = leer.nextLine();
-      estudiante.setNombres(nombres);
-
-      System.out.print("Ingrese los apellidos del estudiante: ");
-      String apellidos = leer.nextLine();
-      estudiante.setApellidos(apellidos);
+      ingresarDatosPersonales(estudiante, leer);
 
       System.out.println("---");
 
-      System.out.print("Ingrese la Materia: ");
-      String materia = leer.nextLine();
-      estudiante.setMateria(materia);
+      ingresarDatosMateria(estudiante, leer);
 
-      System.out.print("Ingrese la nota 1: ");
-      int nota1 = leer.nextInt();
-      estudiante.setNota1(nota1);
 
-      System.out.print("Ingrese la nota 2: ");
-      int nota2 = leer.nextInt();
-      estudiante.setNota2(nota2);
-
-      // Almacenar el objeto Sistema en el array
       estudiantes[i] = estudiante;
 
       double promedio = estudiante.calcularPromedio();
       System.out.println("El promedio del estudiante #" + (i + 1) + " es: " + promedio);
     }
+  }
+
+  private static void ingresarDatosPersonales(Sistema estudiante, Scanner leer) {
+    System.out.print("Ingrese los nombres del estudiante: ");
+    String nombres = leer.nextLine();
+    estudiante.setNombres(nombres);
+
+    System.out.print("Ingrese los apellidos del estudiante: ");
+    String apellidos = leer.nextLine();
+    estudiante.setApellidos(apellidos);
+  }
+
+  private static void ingresarDatosMateria(Sistema estudiante, Scanner leer) {
+    System.out.print("Ingrese la Materia: ");
+    String materia = leer.nextLine();
+    estudiante.setMateria(materia);
+
+    System.out.print("Ingrese la nota 1: ");
+    double nota1 = leer.nextDouble();
+    estudiante.setNota1(nota1);
+
+    System.out.print("Ingrese la nota 2: ");
+    double nota2 = leer.nextDouble();
+    estudiante.setNota2(nota2);
   }
 }
--- Ejercicio_ALumnos_Notas/src/Sistema.java
+++ Ejercicio_ALumnos_Notas/src/Sistema.java
@@ -1,73 +1,60 @@
-public class Sistema {
-    public int CI;
-    public String nombres;
-    public  String apellidos;
+class Sistema {
+    private int ci;
+    private String nombres;
+    private String apellidos;
+    private String materia;
+    private double nota1;
+    private double nota2;
 
-    public String materia;
-
-    public Sistema(int CI) {
-        this.CI = CI;
+    public Sistema(int ci) {
+        this.ci = ci;
     }
 
-    public int getCI() {
-        return CI;
+    public int getCi() {
+        return ci;
     }
 
     public String getNombres() {
         return nombres;
     }
 
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
     public String getApellidos() {
         return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
     }
 
     public String getMateria() {
         return materia;
     }
 
-
-    public void setNombres(String nombres) {
-        this.nombres = nombres;
-    }
-
-    public void setCI(int CI) {
-        this.CI = CI;
-    }
-
-    public void setApellidos(String apellidos){
-        this.apellidos=apellidos;
-    }
-
-
     public void setMateria(String materia) {
         this.materia = materia;
     }
 
-    public int getNota1() {
+    public double getNota1() {
         return nota1;
     }
 
-    public void setNota1(int nota1) {
+    public void setNota1(double nota1) {
         this.nota1 = nota1;
     }
 
-    public int getNota2() {
+    public double getNota2() {
         return nota2;
     }
 
-    public void setNota2(int nota2) {
+    public void setNota2(double nota2) {
         this.nota2 = nota2;
     }
 
-
-
-    public int nota1;
-    public int nota2;
     public double calcularPromedio() {
-        return (double) (getNota1() + getNota2() ) / 2;
+        return (nota1 + nota2) / 2.0;
     }
-    public boolean quedoEnSupletoria() {
-        return calcularPromedio() < 14;
-    }
-
 }
